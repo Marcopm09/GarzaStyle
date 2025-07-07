7c5512672606ca4ff5e88a3eecdf827c0d7f5016
Marcopm09 <pe498785@uaeh.edu.mx>
2025-07-07 17:57:24 -0600

    90%

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome Juan!</ThemedText>
+        <ThemedText type="title">Welcome Juanita!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
