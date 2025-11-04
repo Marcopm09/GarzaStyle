57eabe4e3111678265ec6102e9713d575828e488
Marco Gabriel Perez Montiel <marcoperez119pm@gmail.com>
2025-11-04 09:01:55 -0600

    Cambio facil

--- app/Hora.tsx
+++ app/Hora.tsx
@@ -3,7 +3,7 @@ import React, { createContext, ReactNode, useContext, useEffect, useState } from
 // 1. Crear el contexto
 const HoraContext = createContext('');
 
-// 2. Definir props del proveedor Cambio Prueba
+// 2. Definir props del proveedor Cambio Prueba otto 
 type HoraProviderProps = {
   children: ReactNode;
 };
