393e47bc53f2a60e9aeb5201c1d2f208515d6f7e
azig0 <ivon32kissera@gmail.com>
2025-11-03 21:56:41 -0600

    respaldo con todo y login

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 export default function IndexRedirect() {
   useEffect(() => {
-    router.replace('/Home'); // ← esta es la pantalla que quieres que cargue primero
+    router.replace('/login'); // ← esta es la pantalla que quieres que cargue primero
   }, []);
 
   return null; // No se muestra nada porque redirige automáticamente
